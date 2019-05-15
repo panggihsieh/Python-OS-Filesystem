807da35ec144d6058f43802b1d6fca2d66a23a39
hsiehpanggi <7624220+panggihsieh@users.noreply.github.com>
2019-05-15 11:29:51 +0800

    python list comprehension find subdir ext=png

---
+++ os_walk_extis_png.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import os
+tree=os.walk(r'd:\python data')
+dir_list=[]
+for full in tree:
+    dir_list += [full[0]]
+l=[ os.path.join(dirs,f) for f in os.listdir(dirs) for dirs in dir_list  if f.endswith('png')]
+for i in l:
+    print(i)
