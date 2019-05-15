a7de9949f33789d77fe099492f41b85abfc42909
hsiehpanggi <7624220+panggihsieh@users.noreply.github.com>
2019-05-15 11:42:54 +0800

    Add files via upload

---
+++ os_walk_extis_png_moreconcise.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import os
+dir_list=[ fs[0] for fs in os.walk(r'd:\python data')]
+[ print(os.path.join(dirs,f)) for f in os.listdir(dirs) for dirs in dir_list  if f.endswith('png')]
