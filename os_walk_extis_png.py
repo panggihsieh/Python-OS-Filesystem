1136d1dcc3a21c65d4510408c71e34ac28fb0a03
hsiehpanggi <7624220+panggihsieh@users.noreply.github.com>
2019-05-15 11:32:53 +0800

    concise more

--- os_walk_extis_png.py
+++ os_walk_extis_png.py
@@ -7,8 +7,5 @@ This is a temporary script file.
 import os
 tree=os.walk(r'd:\python data')
 dir_list=[]
-for full in tree:
-    dir_list += [full[0]]
-l=[ os.path.join(dirs,f) for f in os.listdir(dirs) for dirs in dir_list  if f.endswith('png')]
-for i in l:
-    print(i)
+for full in tree: dir_list += [full[0]]
+[ print(os.path.join(dirs,f)) for f in os.listdir(dirs) for dirs in dir_list  if f.endswith('png')]
